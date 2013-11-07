a6cc8b58a5200f6ab7583c53aa58592a0b602688
Armon Shariati <armon.shariati@gmail.com>
2013-11-07 10:14:43 -0500

    added new method with lane bias

---
+++ test2/drive/controller.m
@@ -0,0 +1,112 @@
+function controller()
+
+c = rl_init('controller');
+sub = rl_subscribe('pose');
+
+close all;
+
+
+global kbhit;
+kbhit = false;
+figure('KeyPressFcn', @my_kbhit);
+
+drive_servo = 3;
+steer_servo = 0;
+
+
+encoderPhidgetAPI('c'); 
+con = maestro(); 
+
+len = .3;
+
+v_goal = 1;
+% servo_out = setSpeed(v_goal);
+servo_out = 185; 
+
+v = 0;
+x_pos = 0;
+y_pos = 0;
+theta = 0;
+y_diff = 0;
+
+kp = 2.8;
+kd = 2*sqrt(kp);
+ki = 0;
+
+con.setaccel(drive_servo, 10);
+con.setspeed(drive_servo, 254);
+
+while x_pos < 200 && ~kbhit 
+
+    rl_spin(10);
+    msg = sub.getLatestMessage();
+    if isempty(msg)
+        continue;
+    end
+    pose = msg.data;
+    y_pos = pose(1);
+    theta = pose(2);
+
+    encoderPhidgetAPI('r');
+    
+    [dx, t] = posUpdate();
+    x_pos = x_pos + dx;
+
+    if t ~= 0
+        v = dx/(t/1000);
+    else
+        v = 0;
+    end
+
+    w = (-kd*tan(theta)) - ((kp*(y_pos))/(v*cos(theta)));
+    alpha = atan(len*w/v)*180/pi;
+
+    if abs(w) > 1000
+        alpha = 0;
+    end
+
+
+    alpha = lookup(alpha);
+
+    con.setpos(steer_servo, alpha); 
+    con.setpos(drive_servo, servo_out); 
+end
+
+con.reset(steer_servo);
+con.reset(drive_servo);
+
+encoderPhidgetAPI('d');
+
+end
+
+function [x, t] = posUpdate()
+	
+	[pos, t] = encoderPhidgetAPI('state_update');
+	circ = 0.05*2*pi;
+	ratio = -11/29*3.36; %encoder to wheel
+	x = pos/4096*circ*ratio; %distance since last update
+        
+end
+
+%-51.41 to 42.69
+function pos = lookup(angle)
+angle = angle + 90;
+pos = round((angle - 125.5)/(-0.32));
+if pos > 254
+	pos = 254;
+elseif pos < 0
+	pos = 0;
+end
+end
+
+function p = setSpeed(velocity)
+if (velocity == 10)
+    p = 190;
+else
+    if(velocity == 1)
+        p = 170;
+    end
+end
+end
+
+
---
+++ test2/drive/lane_tracker.m
@@ -0,0 +1,258 @@
+function lane_tracker()
+
+c = rl_init('lane_tracker');
+pub = rl_publish('pose');
+
+close all;
+
+KinectHandles = mxNiCreateContext();
+
+[I, P] = getImageData(KinectHandles);
+
+pic = I;
+I = getBWImage(I);
+
+R = dlmread('rotation');
+T = dlmread('translation');
+
+p_ = transform(R, T, I, P);
+a = [0; 0];
+b = [-500; 500];
+
+X = [1000, 2700];
+Y1 = a(1)*X + b(1);
+Y2 = a(2)*X + b(2);
+
+global count;
+count = 0;
+
+% figure;
+% subplot(2, 2, 1), h1 = imagesc(pic);axis image;
+% subplot(2, 2, 2), h2 = imagesc(I); axis image;
+% colormap gray;
+% subplot(2, 2, 3), h3 = quiver(0, 1, 0, 5, 'r', 'LineWidth', 2); 
+% hold on; plot([0 3], [.5 .5]); plot([0 3], [-.5 -.5]);
+% axis([0 3, -1.5 1.5]);
+% subplot(2, 2, 4), h4 = plot(X, Y1); hold on; plot(X, Y2);
+% axis([0 3000, -1500 1500]);
+
+while 1
+
+    rl_spin(30);
+
+    [I, P] = getImageData(KinectHandles);
+    pic = I;
+    I = getBWImage(I);
+    p_ = transform(R, T, I, P);
+
+    % logger(I, p_);
+
+    X_ = X;
+    [X_, Y1] = biasLane(X, Y1);
+    [Y1, xv1, yv1] = pullLanes(p_, X, Y1);
+    [Y2, xv2, yv2] = pullLanes(p_, X, Y1+1000);
+
+    [y, theta] = getPose(X, Y1, Y2);
+    y = y/1000;
+    y = refineEst(y, theta);
+   
+    pose = [y, theta];
+    msg = Message('pose', pose);
+    pub.publish(msg);
+
+    car_x = .2;
+    car_y = car_x*tan(theta);
+
+    % set(h1,'CDATA', pic);
+    % set(h2, 'CDATA', I);
+    % colormap gray;
+    % set(h3, 'XDATA', 1, 'YDATA', y, 'VDATA', car_y, 'UDATA', car_x); 
+    % axis([0 3, -1.5 1.5]);
+    % cla(h4);
+    % set(h4, 'XDATA', X, 'YDATA', Y1); hold on; plot(X, Y2, 'r'); 
+    % plot(xv1, yv1); plot(xv2, yv2, 'r'); plot(p_(:,1), p_(:,2), '.');
+    % axis([0 3000, -1500 1500]);
+    % drawnow;
+end
+
+end
+
+function b = refineEst(y, theta)
+i = cos(theta);
+j = sin(theta);
+
+m = j/i;
+b = y - m;
+end
+
+function [y, theta] = getPose(X, Y1, Y2)
+i = (X(2)-X(1));
+j1 = (Y1(2)-Y1(1));
+j2 = (Y2(2)-Y2(1));
+
+r1 = [i j1];
+r2 = [i j2];
+r = r1 + r2;
+
+basis = [1 0];
+r = r/norm(r);
+
+theta = acos(dot(basis, r));
+
+m = r(2)/r(1);
+
+if m > 0 
+    theta = -1 * theta;
+end
+
+[a1 b1] = refit([X' Y1']);
+[a2 b2] = refit([X' Y2']);
+
+r_orth = [-1*r(2) r(1)]; 
+m_orth = r_orth(2)/r_orth(1);
+b_orth = -1*m_orth*X(1);
+intcp = [(b1-b_orth)/(m_orth-a1), ((b1-b_orth)/(m_orth-a1)*m_orth) + b_orth; ...
+        (b2-b_orth)/(m_orth-a2), ((b2-b_orth)/(m_orth-a2)*m_orth) + b_orth];
+
+width = dist(intcp);
+d_bot = dist([X(1), 0; intcp(1, :)]);
+d_top = dist([X(1), 0; intcp(2, :)]);
+
+
+% y = 500 - 1000*(d_top/width);
+y = 500 - d_top*(width/1000);
+
+end
+
+function [X_, Y_] = biasLane(X, Y)
+    theta = pi/18;
+    R = [cos(theta) sin(theta); -sin(theta) cos(theta)];
+    P_ = R*[X' Y'];
+    P_ = P_';
+    X_ = P_(1,:);
+    Y_ = P_(2,:) + 10;
+end
+
+function [Y, xpnts, ypnts] = pullLanes(data, X, Y)
+
+    % xpnts = [X(1)-250 X(1)-250 X(2)+250 X(2)+250];
+    % ypnts = [Y(1)+100 Y(1)-100 Y(2)-100 Y(2)+100];
+    xpnts = [X(1)-150 X(1)-150 X(2)+150 X(2)+150];
+    ypnts = [Y(1)+150 Y(1)-150 Y(2)-150 Y(2)+150];
+
+    inliers_mask = inpolygon(data(:,1), data(:,2), xpnts, ypnts);
+    inliers = data(inliers_mask,:);
+    
+    [a, b] = ransac(inliers);
+    
+    Y = a*X + b;
+end
+
+
+function [a, b] = ransac(data)
+n = 2;
+k = 50;
+t = 50;
+
+c_set = [];
+maxsize = 0;
+
+
+for i=1:k
+    idc = randperm(size(data,1), n);
+    p1 = data(idc(1),:);
+    p2 = data(idc(2),:);
+    a = (p2(2)-p1(2))/(p2(1)-p1(1));
+    b = p2(2) - a*p2(1);
+    
+    y = a*data(:,1) + b;
+    e = abs(data(:,2) - y);
+
+    mask = (e < t);
+    e = e.*mask;
+    len = size(unique(e), 1) - 1;
+
+    if len > maxsize 
+        X = data(:,1).*mask; 
+        Y = data(:,2).*mask; 
+        c_set = [X Y];
+        maxsize = len;
+    end
+end
+
+c_set = unique(c_set, 'rows');
+c_set = setdiff(c_set, [0 0], 'rows');
+[a, b] = refit(c_set);
+
+end
+
+function p = transform(R, T, I, P)
+X = P(:,:,1).*I;
+Y = P(:,:,2).*I;
+Z = P(:,:,3).*I;
+
+d_mask = (Z < 2500);
+X = X.*d_mask;
+Y = Y.*d_mask;
+Z = Z.*d_mask;
+
+X = reshape(X, 1, 240*320);
+Y = reshape(Y, 1, 240*320);
+Z = reshape(Z, 1, 240*320);
+
+ps = [X; Y; Z];
+ps_ = R'*ps; 
+ps_ = ps_';
+
+ps_(:,1) = ps_(:,1) + T(1);
+ps_(:,2) = ps_(:,2) + T(2);
+
+ps_ = [ps_(:,1) ps_(:,2)];
+ps_ = unique(ps_, 'rows');
+ps_ = setdiff(ps_, [0 0], 'rows');
+
+p = ps_;
+
+end
+
+function [a, b] = refit(data)
+x = data(:,1);
+y = data(:,2);
+
+C = [x ones(size(x))];
+p = C\y;
+
+a = p(1);
+b = p(2);
+end
+
+function r = dist(pts)
+
+    r = sum((pts(2, :) - pts(1, :)).^2);
+    r = r.^(1/2);
+
+end
+
+
+function bw = getBWImage(I)
+I = rgb2gray(I);
+bw = edge(I, 'sobel');
+bw(120:end, :) = 0;
+bw(1:50, :) = 0;
+end
+
+function [I, P] = getImageData(KinectHandles)
+I = getRGBImage(KinectHandles);
+P = double(getPointCloud(KinectHandles));
+end
+
+function logger(I, P);
+global count;
+folder_name = './log';
+image_file = strcat(folder_name, '/i',num2str(count),'.png');
+pc_file = strcat(folder_name,'/i',num2str(count),'.mat');
+
+imwrite(I, image_file, 'png');
+save(pc_file, 'P');
+count = count + 1;
+end
